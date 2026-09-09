1a6de68a13246ec67cf531339e0ab3c7da9b3405
Your Name <you@example.com>
2026-09-09 22:29:26 +0530

    Connect frontend to deployed backend

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function loginUser(event) {
     const password = document.getElementById("loginPassword").value;
 
     try {
-        const response = await fetch("http://localhost:5000/api/users/login", {
+        const response = await fetch("http://https://blog-application-69lz.onrender.com/api/users/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"

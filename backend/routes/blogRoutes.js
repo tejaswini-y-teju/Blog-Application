939dbe52034b45584fd9a0702a1f3d9081551edb
Your Name <you@example.com>
2026-09-09 22:21:16 +0530

    Fix Blog model filename

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Blog = require("../models/Blog");
+const Blog = require("../models/blog");
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
